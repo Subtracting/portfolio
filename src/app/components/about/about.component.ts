bf68b30f21b3681b86e46cebf812fbe710b127c4
Subtracting <subtracting@gmail.com>
2024-05-18 22:11:31 +0200

    'particles'

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  aboutArray: Array<string> = [
+    "Hey, I'm [Artist's Name]. By profession, I'm a data analyst, known for my problem-solving skills. Off the clock, I express myself through abstract art. My pieces reflect my analytical approach to life, blending emotion with logic. I enjoy creating enigmatic faces that challenge perceptions and inspire curiosity, fusing my passion for art with my love for problem-solving.",
+    "Jan-Willem van der Boom, 35 years old. Data analyst. I have a passion for problem-solving, puzzles and patterns. Both art and coding allow me to express and challenge myself. @subtracting",
+    "Hey, I'm [Artist's Name]. By profession, I'm a data analyst, crunching numbers and solving problems. In my spare time, I'm equally passionate about coding and painting abstract faces. Both art and coding provide me with avenues for creative problem-solving and expression.",
+    "Hey, I'm [Artist's Name]. By day, I'm a data analyst, solving puzzles with numbers. Off-hours, I'm coding or painting abstract faces. Both art and coding are my canvas for problem-solving and creativity."
+  ];
+
+  aboutText: string = '';
+
+  ngOnInit() {
+    this.selectAboutText();
+  }
+
+  selectAboutText() {
+    this.aboutText = this.aboutArray[Math.floor(Math.random() * this.aboutArray.length)]
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
-import { ArtComponent } from '../art/art.component';
-import { ProjectsComponent } from '../projects/projects.component';
-import { AboutComponent } from '../about/about.component';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -21,9 +19,21 @@ export class HomeComponent {
   frequency: number = 600;
   appearVarX: number = 0;
   appearVarY: number = 0;
-  followX: number = 500;
-  followY: number = 500;
+  startX: number = 700;
+  startY: number = Math.random() * 1000;
+  followArray: Array<{ x: number, y: number }> = [
+    {x: 500, y: 500},
+    {x: 1200, y: 200},
+    {x: 1200, y: 800},
+    {x: 200, y: 200},
+    {x: 700, y: 800},
+    {x: 750, y: 500}
+  ];
+
   word: string = "SUBTRACTING.";
+  points: Array<{ x: number, y: number }> = [];
+  rotation: number = 0;
+  direction: number = 1;
 
   @ViewChild('titleCanvas', { static: false, read: ElementRef})
   titleCanvas!: ElementRef;
@@ -50,40 +60,106 @@ export class HomeComponent {
       this.context2 = this.gridCanvas.nativeElement.getContext("2d");
       this.context2.canvas.height = window.innerHeight;
       this.context2.canvas.width = window.innerWidth;
+      this.generatePoints(this.startX, this.startY);
       this.animateGrid();
+      this.changeDirection();
     }
+
+  }
+
+  generatePoints(startX: number, startY: number) {
+    const count = 10000;
+    const radius = this.context2.canvas.width;
+
+    for (let i = 0; i < count; i++) {
+      const angle = Math.random() * 2 * Math.PI;
+      const distance = 10 + Math.random() * radius;
+      const x = startX + distance * Math.tan(angle);
+      const y = startY + distance ;
+      this.points.push({ x, y });
+    }
+      for (var i = 0; i < 10000; i ++) {
+        const x = Math.random() * this.context2.canvas.width;
+        const y = Math.random() * this.context2.canvas.height;
+        this.points.push({ x, y });
+      }
+
+  }
+
+  movePoints(followArray: Array<{ x: number, y: number }>) {
+    const newPoints: Array<{ x:number, y: number }> = [];
+    let toX = 0;
+    let toY = 0;
+
+    for (const point of this.points) {
+      let smallestDist = 99999;
+
+      for (const follow of followArray) {
+        const dx = follow.x - point.x;
+        const dy = follow.y - point.y;
+        const dist = Math.sqrt(dx*dx + dy*dy);
+
+        if (Math.random() * dist < smallestDist) {
+          smallestDist = dist;
+          toX = follow.x;
+          toY = follow.y;
+        };
+
+      };
+
+      const dx = toX - point.x;
+      const dy = toY - point.y;
+
+      const dist = Math.sqrt(dx*dx + dy*dy);
+
+      const ux = dx / dist;
+      const uy = dy / dist;
+
+      const movedX = point.x - uy;
+      const movedY = point.y + ux;
+      newPoints.push({x: movedX, y: movedY});
+    };
+
+    this.points = newPoints;
+  }
+
+  changeDirection() {
+    interval(10000).subscribe(_ => {
+      this.direction *= -.1;
+      this.rotation += 1;
+
+      this.moveAttractor(this.followArray);
+    });
+  }
+
+  moveAttractor(followArray: Array<{ x: number, y: number }>) {
+    const newFollowArray: Array<{ x: number, y: number }> = [];
+    for (var j = 0; j < followArray.length; j++) {
+      const newFollowX = Math.random() * 2000;
+      const newFollowY = Math.random() * 1000;
+
+      newFollowArray.push({ x: newFollowX, y: newFollowY });
+    }
+
+    this.followArray = newFollowArray;
   }
 
   animateGrid() {
     setTimeout(()=> {
       requestAnimationFrame(this.animateGrid.bind(this));
-      const step = 40;
+
       this.context2.clearRect(0, 0, this.context2.canvas.width, this.context2.canvas.height);
-      this.context2.strokeStyle = 'grey';
-      for (var x = 0; x < this.context2.canvas.width; x += step) {
-        for (var y = 0; y < this.context2.canvas.height; y += step) {
-          const dx = this.followX - x;
-          const dy = this.followY - y;
-
-          const dist = Math.sqrt(dx*dx + dy*dy);
-
-          const ux = dx / dist;
-          const uy = dy / dist;
-
-          const x2 = x + ux * Math.min(50, 220/dist);
-          const y2 = y + uy * Math.min(50, 50/dist);
-
-          this.context2.beginPath();
-          this.context2.moveTo(x, y);
-          this.context2.lineTo(x2, y2);
-          this.context2.stroke();
-          this.context2.closePath();
+      for (const point of this.points) {
+        if ((point.x <= this.context2.canvas.width) && (point.y <= this.context2.canvas.height)) {
+          this.context2.fillStyle = `rgb(200 200 200 / ${100/(point.x/point.y)}%)`;
+          this.context2.fillRect(point.x, point.y, 1, 1);
         }
+
       }
+      this.movePoints(this.followArray);
     }
     , 10);
-  this.followX += Math.random()*8*(Math.round(Math.random())*2 - 1);
-  this.followY += Math.random()*8*(Math.round(Math.random())*2 - 1);
+
   }
 
   animateSineWave(word: string) {
@@ -95,7 +171,7 @@ export class HomeComponent {
       this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
       this.context.beginPath();
       for (var x = 0; x < this.n; x ++) {
-        var y = 50 * (this.context.canvas.width / (10000-this.appearVarY)) * Math.tan(40000/this.appearVarX / this.amplitude * x);
+        var y = 450 * (this.context.canvas.width / (10000-this.appearVarY)) * Math.sin(40000/this.appearVarX / this.amplitude * x);
         this.context.lineTo(x-20, y + 60);
       }
       this.context.closePath();
